3f57c4850e8d797245e72333abcb4b1f1fcfdfb7
Rickin Shah <rickin.shah17403@gmail.com>
2025-05-12 08:47:07 +0530

    adds small improvement for user experience

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,10 +1,11 @@
 alias ls='eza -1 --color=always --icons=always --hyperlink --group-directories-first'
 alias cat='bat'
+direnv hook fish | source
 if status is-interactive
     # Commands to run in interactive sessions can go here
     set -g fish_greeting
-    direnv hook fish | source
-    # pokemon-colorscripts --no-title -r
     fastfetch
-
+    stty -icanon -echo
+    dd bs=1 count=1 >/dev/null 2>&1
+    stty icanon echo
 end
